e8d669d2816f000fa631b24d8bb3171e2ec9a767
7y58mq2w5z-ops <7y58mq2w5z@privaterelay.appleid.com>
2026-06-04 01:02:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,10 +9,6 @@ const ASSETS = [
   './manifest33.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
-  'https://cdn.tailwindcss.com',
-  'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.css',
-  'https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js',
-  'https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js'
 ];
 
 self.addEventListener('install', (event) => {
